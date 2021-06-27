09dfda7d80c5906ca0ace6d171061936a3b3b19b
Dementor109 <84393978+Dementor109@users.noreply.github.com>
2021-06-27 19:36:13 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var block, block2, block3;
+
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    block = new Stone(600,500);
+    block2 = new Rubber(700,200,10);
+    block3 = new Iron(200,400);
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    block.display();
+    block2.display();
+    block3.display();
+ 
+}
